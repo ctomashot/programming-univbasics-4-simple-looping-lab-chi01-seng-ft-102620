4ec4191885074a68e0486bccc66350eaafa19df3
James Nicholas Tomashot <ctomashot@gmail.com>
2020-10-14 21:14:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,7 +9,11 @@ def loop_message_five_times (string)
 end
 
 def loop_message_n_times (string, n)
-  
+    counter = 1
+  while counter < n + 1
+   puts string
+   counter += 1
+  end
 end
 
 def output_array
