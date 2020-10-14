ff84741c37e83e2bbf3ded0833ad96ec449056bc
James Nicholas Tomashot <ctomashot@gmail.com>
2020-10-14 21:19:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,5 +25,10 @@ def output_array (array)
 end
 
 def return_string_array (array)
-  
+  counter = 0
+  string_array = []
+  while counter < array.length
+   string_array.push (array[counter].to_s)
+   counter += 1
+  end
 end
